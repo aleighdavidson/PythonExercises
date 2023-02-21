f0fa6249c320e9ab9e31ff93c07cfc5ab5f53cfb
Alex Davidson <alex.leigh.davidson@gmail.com>
2023-02-21 19:11:31 +0000

    minor update to comments after tutorial

--- ex12_Q1.py
+++ ex12_Q1.py
@@ -1,10 +1,11 @@
 cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
 # this adds each letter as a separate item to the list
 # cheese += 'Oke'
+# because it thinks it is a tuple
+
 # cheese.append(['Oke', 'Brie']) will add the two together in the square brackets as a single item to the list
 # cheese.extend(['Oke', 'Brie'])
 print(cheese)
 # this worked though
 cheese += ['Oke', 'Brie']
 print(cheese)
-# need the square brackets to tell it it's a list? why is it making the string separate elements for each letter?
